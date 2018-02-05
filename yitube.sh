c9e193436b46be83d4727c9c761a58d589169779
Satoru Yoshihara <vaduz0@gmail.com>
2018-02-06 00:44:24 +0900

    Skip concatenating movies when file is already exists

--- yitube.sh
+++ yitube.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Started at $(date)"
 
 SDCARD_DIR=${SDCARD_DIR:-"/Volumes/Untitled/DCIM/100MEDIA"}
 
@@ -12,9 +13,18 @@ OUTPUT_FILE_PATH=${OUTPUT_FILE_PATH:-${SSD_PATH}/${OUTPUT_TITLE}.mp4}
 
 find "$SDCARD_DIR" -type f -name "*.MP4" | perl -nE 'print "file $_"' > "${INPUT_MEDIA_META}"
 
-time ffmpeg -safe 0 -f concat -i "${INPUT_MEDIA_META}" -c copy "${OUTPUT_FILE_PATH}"
+if [[ -f "${OUTPUT_FILE_PATH}" ]]
+then
+    echo "Skipping. File already exists: '${OUTPUT_FILE_PATH}'"
+else
+    echo "Concatenating movies to '${OUTPUT_FILE_PATH}'. Input media meta data '${INPUT_MEDIA_META}'."
+    time ffmpeg -safe 0 -f concat -i "${INPUT_MEDIA_META}" -c copy "${OUTPUT_FILE_PATH}"
+fi
 
 CLIENTS_SECRETS=${CLIENTS_SECRETS:-"/Users/Vaduz/Codes/youtube-upload/client_secrets.json"}
 VIDEO_PRIVACY=${VIDEO_PRIVACY:-unlisted}
 
-youtube-upload --client-secrets=${CLIENTS_SECRETS} --title="${OUTPUT_TITLE}" --privacy=${VIDEO_PRIVACY} "${OUTPUT_FILE_PATH}"
+echo "Executing: youtube-upload --client-secrets=${CLIENTS_SECRETS} --title=${OUTPUT_TITLE} --privacy=${VIDEO_PRIVACY} \"${OUTPUT_FILE_PATH}\""
+youtube-upload --client-secrets=${CLIENTS_SECRETS} --title=${OUTPUT_TITLE} --privacy=${VIDEO_PRIVACY} "${OUTPUT_FILE_PATH}"
+
+echo "Finished at $(date)"
