82299e37363a59b1750aed874bdbfe9398232ae8
Satoru Yoshihara <vaduz0@gmail.com>
2018-02-09 00:49:20 +0900

    Change output format to mkv from mp4

--- yitube.sh
+++ yitube.sh
@@ -9,7 +9,7 @@ CAPTURED_DATE=${CAPTURED_DATE:-$(stat -t %Y%m%d $(ls -ltr -1 *.MP4 | head -1) | 
 INPUT_MEDIA_META="/tmp/${CAPTURED_DATE}.txt"
 SSD_PATH=${SSD_PATH:-"/Volumes/SanDisk Ultra II 960GB"}
 OUTPUT_TITLE=${OUTPUT_TITLE:-${CAPTURED_DATE}卓球}
-OUTPUT_FILE_PATH=${OUTPUT_FILE_PATH:-${SSD_PATH}/${OUTPUT_TITLE}.mp4}
+OUTPUT_FILE_PATH=${OUTPUT_FILE_PATH:-${SSD_PATH}/${OUTPUT_TITLE}.mkv}
 
 find "$SDCARD_DIR" -type f -name "*.MP4" | perl -nE 'print "file $_"' > "${INPUT_MEDIA_META}"
 
